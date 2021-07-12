39211b70916a1793b34a433a267c9dea3ed0f8a0
Manisha Goyal <manisha.goyal@allscripts.com>
2021-07-12 17:54:21 +0530

    changes in locals.py and settings.json

--- configcenter/locals.py
+++ configcenter/locals.py
@@ -2,8 +2,8 @@ import os
 PROJECT_DIR =  os.path.join(os.path.dirname(__file__), '../')
 debug = True
 version = '7.0' 
-db_name = 'dbmCCenter_NewDjango' #'dbmCCenterAlla_Test'
-db_host = 'ph-aio-64\INST01' #'du-cc-sql\INST01' 
+db_name = 'MyCCenterDB'#'dbmCCenter_NewDjango' #'dbmCCenterAlla_Test'
+db_host = 'myccenterserver.database.windows.net' #'du-cc-sql\INST01' 
 dbm_shared_folder = os.path.realpath(os.path.join(PROJECT_DIR, 'shared_folder_dev')) 
 usage_reporting_server = 'dbm-cc-aio1'
 usage_reporting_path = r'c:\Program Files\Microsoft SQL Server\MSRS11.INST02\Reporting Services'
